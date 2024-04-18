be6c910c6ad1b1b2980708c299bf8f398a7c4b57
macstudio <macos@example.com>
2024-04-18 21:01:34 +0300

    Initial Commit

---
+++ SwiftUI-Deep-Links-TabView-NavigationStack/SwiftUI_Deep_Links_TabView_NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Deep_Links_TabView_NavigationStackApp.swift
+//  SwiftUI-Deep-Links-TabView-NavigationStack
+//
+//  Created by YILMAZ ER on 18.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Deep_Links_TabView_NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
